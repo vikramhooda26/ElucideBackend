f73395e52b62e01b04a564935242c95b86e6f9fd
Anirudh Memani <nemanianirudh@gmail.com>
2024-06-16 01:36:50 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+const authorizedHosts = process.env.AUTHORIZED_HOSTS
+    ? process.env.AUTHORIZED_HOSTS.split(",")
+    : [];
+
+app.use(
+    cors({
+        origin: authorizedHosts,
+    })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () =>
+    console.log(`${new Date().toLocaleTimeString()} Listening on port ${port}`)
+);
