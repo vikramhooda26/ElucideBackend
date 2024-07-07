0ac4ec0d1294d02aecacc6155fc447bccf4caab2
Anirudh Memani <nemanianirudh@gmail.com>
2024-07-08 00:06:11 +0530

    code cleaning

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import { prisma } from "./db/index.js";
 
 dotenv.config();
 const app = express();
@@ -19,25 +18,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// async function main() {
-//   try {
-//     await prisma.dashapp_subcategory.createMany({
-//       data: [
-//         { subcategory: "TV Channels", category_id: 39 },
-//         { subcategory: "Radio", category_id: 39 },
-//         { subcategory: "Print", category_id: 39 },
-//         { subcategory: "OOH", category_id: 39 },
-//         { subcategory: "Social Media Platforms", category_id: 39 },
-//       ],
-//     });
-//     console.log("successfully added categories");
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// await main();
-
-// app.listen(port, () =>
-//   console.log(`${new Date().toLocaleTimeString()} Listening on port ${port}`),
-// );
+app.listen(port, () =>
+  console.log(`${new Date().toLocaleTimeString()} Listening on port ${port}`),
+);
