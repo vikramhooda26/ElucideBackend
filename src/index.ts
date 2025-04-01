349bf9afc37fd579f69704ded0ecfd7a1b300574
AnirudhMemani <nemanianirudh@gmail.com>
2025-04-01 07:42:52 +0530

    chore: remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import { Request, Response } from "express";
-import { prisma } from "./db/index.js";
 import { cookieParser, cors, dotenv, express } from "./imports/core.imports.js";
 import { authMiddleware, globalErrorHandler } from "./imports/middleware.imports.js";
 import * as Routers from "./imports/router.imports.js";
-import { STATUS_CODE } from "./lib/constants.js";
-import { InternalServerError } from "./lib/errors.js";
-import { printLogs } from "./lib/log.js";
 
 (BigInt.prototype as any).toJSON = function () {
     return this.toString();
@@ -13,40 +8,9 @@ import { printLogs } from "./lib/log.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT || "3000");
 
 const authorizedHosts = process.env.AUTHORIZED_HOSTS ? process.env.AUTHORIZED_HOSTS.split(",") : [];
-
-app.get("/main-categories", async (_req: Request, res: Response) => {
-    try {
-        const result = await prisma.dashapp_category.findMany({
-            select: {
-                id: true,
-                category: true,
-                dashapp_subcategory: {
-                    select: {
-                        id: true,
-                        subcategory: true,
-                        dashapp_companydata_subcategory: {
-                            select: {
-                                dashapp_companydata: {
-                                    select: {
-                                        id: true,
-                                        company_name: true,
-                                    },
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-        });
-        res.status(STATUS_CODE.OK).json(result);
-    } catch (error) {
-        printLogs("main-categories | ERROR", error);
-        throw new InternalServerError("Something went wrong");
-    }
-});
 
 app.use(
     cors({
